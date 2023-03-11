bcc55f80d01c41e2a6942f8443136098ceaeaa5d
Kaushal <Kaushalsingh8178@gmail.com>
2023-03-11 19:25:50 +0530

    implement client side

---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from 'axios';
+import Card from "../components/Card";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Home =({type})=>{
+  const [videos,setVideos]=useState([]);
+  useEffect(()=>{
+    const fetchVideos=async ()=>{
+      const res=await axios.get(`/videos/${type}`);
+      setVideos(res.data);
+    };
+    fetchVideos();
+  },[type]
+  );
+  
+  return (
+    <Container>
+      {videos.map((video) => (
+        <Card key={video._id} video={video}/>
+      ))}
+    </Container>
+  );
+}
+
+
+
+
+export default Home;
